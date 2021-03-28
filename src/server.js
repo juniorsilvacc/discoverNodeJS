4128043a1a25de5b3d1fba2a3cc846afa9e5decd
juniorsilvaeng <juniorsilvacc@hotmail.com>
2021-03-28 01:08:07 -0300

    routes

--- src/server.js
+++ src/server.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const app = express();
 
+const routes = require('./routes')
 
+//Habilitar arquivos statics
+app.use(express.static("public"));
+
+//Rotas
+app.use(routes); 
 
 app.listen(3500, () => {
   console.log('Run server!')
