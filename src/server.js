e323bd4077bf6f62ba21f8f232c4db5d9eee59d4
juniorsilvaeng <juniorsilvacc@hotmail.com>
2021-04-01 15:40:25 -0300

    function remainingDays

--- src/server.js
+++ src/server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
-
 const routes = require('./routes');
 
 app.set('view engine', 'ejs')
 
 //Habilitar arquivos statics
 app.use(express.static("public"));
+
+//Liberando o req.body
+app.use(express.urlencoded({extended: true}));
 
 //Rotas
 app.use(routes); 
