c0a4280a0451ac48b4cc1fa8c37ecf573ad00b13
juniorsilvaeng <juniorsilvacc@hotmail.com>
2021-04-13 23:32:16 -0300

    dashbordController

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,9 @@ const express = require('express');
 const routes = express.Router();
 const ProfileController = require('./controllers/ProfileController');
 const JobController = require('./controllers/JobController');
+const DashbordController = require('./controllers/DashbordController');
 
-routes.get("/", JobController.index);
+routes.get("/", DashbordController.index);
 routes.get("/job", JobController.create);
 routes.post("/job", JobController.save);
 routes.get("/job/:id", JobController.show);
--- src/utils/JobUtils.js
+++ src/utils/JobUtils.js
@@ -1,20 +1,20 @@
 module.exports = {
-  remainingDays(job){
-  //Calculo de tempo restante
-  const remainingDays = (job["total-hours"] / job["daily-hours"]).toFixed();
+  remainingDays(job) {
+    //Calculo de tempo restante
+    const remainingDays = (job["total-hours"] / job["daily-hours"]).toFixed();
 
-  const createdDate = new Date(job.created_at);
-  const dueDay = createdDate.getDate() + Number(remainingDays);
-  const dueDateInMs = createdDate.setDate(dueDay);
+    const createdDate = new Date(job.created_at);
+    const dueDay = createdDate.getDate() + Number(remainingDays);
+    const dueDateInMs = createdDate.setDate(dueDay);
 
-  const timeDiffInMs = dueDateInMs - Date.now();
+    const timeDiffInMs = dueDateInMs - Date.now();
 
-  //Transformar Millisegundos em dias
-  const dayInMs = 1000 * 60 * 60 * 24;
-  const dayDiff = Math.floor(timeDiffInMs / dayInMs);
+    //Transformar Millisegundos em dias
+    const dayInMs = 1000 * 60 * 60 * 24;
+    const dayDiff = Math.floor(timeDiffInMs / dayInMs);
 
-  //Restam x dias
-  return dayDiff;
-},
-  calculateBudget: (job, valueHour) => valueHour * job['total-hours']
-}
+    //Restam x dias
+    return dayDiff;
+  },
+  calculateBudget: (job, valueHour) => valueHour * job["total-hours"],
+};
